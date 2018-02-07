3b1f12f5bc5c47b9307248beca47ab84bd0cd90e
johntalton <johntalton@gmail.com>
2018-02-07 15:35:28 +0000

    duh

--- examples/test.js
+++ examples/test.js
@@ -1,5 +1,5 @@
 const rasbus = require('rasbus');
-const mcp300X = require('./src/mcp300X.js');
+const mcp300X = require('../src/mcp300X.js');
 
 rasbus.pispi.init('/dev/spidev0.0').then(spi => {
   return mcp300X.adc({ bus: spi, Vref: 5.0, channels: 8 }).then(dev => {
